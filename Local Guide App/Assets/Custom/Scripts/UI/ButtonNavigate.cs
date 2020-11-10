c610105d1e6d4bf95e5b513e03683dc307b49215
Vinod <vnod52@gmail.com>
2020-11-10 14:29:09 +1300

    add share button function, setup spacing for attraction and region panels

--- Local Guide App/Assets/Custom/Scripts/UI/ButtonNavigate.cs
+++ Local Guide App/Assets/Custom/Scripts/UI/ButtonNavigate.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using System.IO;
+using System.Collections;
 
 public class ButtonNavigate : MonoBehaviour {
     public Animator animator;
+
+    private string shareMessage;
 
     //Open a panel, run open animation
     public void Open() {
@@ -16,10 +20,31 @@ public class ButtonNavigate : MonoBehaviour {
     //Open attraction addresss in google maps
     public void OpenMaps() {
         Application.OpenURL(AttractionDetailPanel.Instance.txtAddress);
-    }  
-    
+    }
+
     //Share attraction to friends
     public void Share() {
-        //Application.OpenURL("https://goo.gl/maps/7WX6xbmtQMX7kox27");
+        shareMessage = "Check this out! " + AttractionDetailPanel.Instance.txtAddress;
+        StartCoroutine(TakeScreenshotAndShare());
+    }
+
+    //Native share method displays message and screenshot of app;
+    private IEnumerator TakeScreenshotAndShare() {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath)
+            .SetSubject("NZ Local Guide").SetText(shareMessage)
+            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .Share();
     }
 }
